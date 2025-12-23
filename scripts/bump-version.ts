5c3ba2db797a434df5ad1bb4c92ca8ab1f02fc89
Yuuki Rika <yvvki@kekkon.nexus>
2025-12-23 19:34:26 +0700

    build: refactor bump-version

--- scripts/bump-version.ts
+++ scripts/bump-version.ts
@@ -1,7 +1,6 @@
 import pkg from "@kekkon-nexus/config/package.json" with { type: "json" };
 
 const [major, minor, patch = 0] = pkg.version.split(".").map(Number);
-const prev = Number(minor);
 
 const d = new Date();
 const yy = d.getUTCFullYear() - 2000;
@@ -9,11 +8,10 @@ const mm = d.getUTCMonth() + 1;
 const dd = d.getUTCDate();
 const next = yy * 10000 + mm * 100 + dd;
 
-const version = [major, next, prev === next ? patch + 1 : 0].join(".");
+const version = [major, next, next === minor ? patch + 1 : 0].join(".");
 
 const proc = Bun.spawn(["bun", "pm", "version", version], {
 	stdout: "inherit",
 	stderr: "inherit",
 });
-await proc.exited;
-process.exit(proc.exitCode ?? 1);
+process.exit(await proc.exited);
